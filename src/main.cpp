c0ac56df1235f3d1673f774c82bc85bacf6b6c49
Vitezslav Kanok <vitezslav.kanok@email.cz>
2021-11-09 12:00:59 +0100

    brute force first attempt

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,24 @@
 #include <fstream>
 #include <iostream>
 #include <vector>
+#include <math.h>
+#include <string>
 #include <algorithm>
+#include <cmath>
+using namespace std;
 
+class DiffPos {
+public:
+    float diff;
+    int pos;
+    bool operator< (const DiffPos& other) const {
+        return diff < other.diff;
+    }
+    DiffPos(float diff, int pos) {
+        this->diff = diff;
+        this->pos = pos;
+    }
+};
 
 void sift_test();
 
@@ -20,35 +36,59 @@ void brute_force(float* data, float* query, std::vector<int>& result, size_t dim
     // TODO
     // scan the data and compute a distance between each vector and query
     // store K nearest neighbors in the result vector
+    float diff = 0;
+    int qI = 0;
+    //float* res = new float[100 * dim];
+    std::vector<DiffPos> listOfDiffPos;
+    size_t size = 0;
+    for (int i = 0; i < node_count; i++) {
+        diff += powf((data[i] - query[qI]), 2);
+        qI++;
+        if (i % dim == 0) {
+            listOfDiffPos.push_back(DiffPos(sqrtf(diff), size++));
+            qI = 0;
+            diff = 0;
+        }
+    }
+
+    // Sort results 
+    std::sort(listOfDiffPos.begin(), listOfDiffPos.end());
+
+    // Get result indexes from sorted result
+    for (int i = 0; i < k; i++) {
+        cout << "Position[" << i << "] = " << listOfDiffPos[i].pos << "\n";
+        result.push_back(listOfDiffPos[i].pos);
+    }
 }
 
 void sift_test() {
     //vectors count
     size_t node_count = 1000000;
     //query count
-    size_t qsize = 10000;
+    size_t qsize = 1;
     //vector dimension
     size_t vecdim = 128;
     //answers count for 1 query
-    size_t answer_size = 100;
+    size_t answer_size = 1;
     //numberr of nearest neigbors to find
-    uint32_t k = 10;
-
+    uint32_t k = 1;
+    
+   
     /////////////////////////////////////////////////////// READ DATA
     float* mass = new float[node_count * vecdim];
-    std::ifstream input("sift1M/sift1M.bin", std::ios::binary);
+    std::ifstream input("c:/All/VSB/2rocnik/AVD/sift1M/sift1M.bin", std::ios::binary);
     if (!input.is_open()) std::runtime_error("Input data file not opened!");
     input.read((char*)mass, node_count * vecdim * sizeof(float));
     input.close();
 
     float* massQ = new float[qsize * vecdim];
-    std::ifstream inputQ("sift1M/siftQ1M.bin", std::ios::binary);
+    std::ifstream inputQ("c:/All/VSB/2rocnik/AVD/sift1M/siftQ1M.bin", std::ios::binary);
     if (!input.is_open()) std::runtime_error("Input query file not opened!");
     inputQ.read((char*)massQ, qsize * vecdim * sizeof(float));
     inputQ.close();
 
     unsigned int* massQA = new unsigned int[qsize * answer_size];
-    std::ifstream inputQA("sift1M/knnQA1M.bin", std::ios::binary);
+    std::ifstream inputQA("c:/All/VSB/2rocnik/AVD/sift1M/knnQA1M.bin", std::ios::binary);
     if (!input.is_open()) std::runtime_error("Input result file not opened!");
     inputQA.read((char*)massQA, qsize * answer_size * sizeof(int));
     inputQA.close();
@@ -70,6 +110,7 @@ void sift_test() {
         for (int i = 0; i < qsize; i++)
         {
             brute_force(mass, &massQ[i * vecdim], result, vecdim, k, node_count);
+            cout << "Act r " << massQA[0];
         }
         auto end = std::chrono::steady_clock::now();
         int time = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
@@ -85,4 +126,6 @@ void sift_test() {
     delete[] mass;
     delete[] massQ;
     delete[] massQA;
+
+    
 }
